ff8e5c748cc097eabcf26b0b69d4c2eb37e9f41b
Mshahas786 <mshahas786@gmail.com>
2021-05-17 16:09:44 +1200

    Updates

---
+++ precache-manifest.7b3c25ef13ddbef708d90f72bbb76aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d9d64c31761371a09cf",
+    "url": "/portfolioReact/static/js/main.42b0f1c8.chunk.js"
+  },
+  {
+    "revision": "9028f188735ef4f1d2ea",
+    "url": "/portfolioReact/static/js/runtime~main.af92f141.js"
+  },
+  {
+    "revision": "65d27b8998e3133b6044",
+    "url": "/portfolioReact/static/css/2.0340539f.chunk.css"
+  },
+  {
+    "revision": "65d27b8998e3133b6044",
+    "url": "/portfolioReact/static/js/2.e48b5966.chunk.js"
+  },
+  {
+    "revision": "4b298c6d7a8ae745a5ed9b1696e58641",
+    "url": "/portfolioReact/index.html"
+  }
+];
