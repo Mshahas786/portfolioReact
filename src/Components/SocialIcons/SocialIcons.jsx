7924ed28b3f8fe7439400a7582cf2bbb19dd2732
Muhammed shahas <Mshahas7886@gmail.com>
2021-05-27 12:14:16 +1200

    hello

--- src/Components/SocialIcons/SocialIcons.jsx
+++ src/Components/SocialIcons/SocialIcons.jsx
@@ -10,9 +10,9 @@ const SocialIcons = () => {
 
   return (
     <div className="SocialIcons d-flex flex-lg-column justify-content-center">
-     <motion.div whileHover={{ scale: 1.4 }}> <SocialIcon  className="m-2" bgColor="#ffc107" fgColor="#000" url="https://github.com/Mshahas786"/></motion.div>
-     <motion.div whileHover={{ scale: 1.4 }} onMouseEnter={() => setShow(!show)} onMouseLeave={() => setShow(!show)} ><SocialIcon className="m-2" bgColor="#ffc107" fgColor={ show ?  "#2164f4" : "#000" } url="https://www.linkedin.com/in/muhammed-shahas" /></motion.div>
-     <motion.div whileHover={{ scale: 1.4 }}  ><SocialIcon onMouseEnter={() => setTwitter(!twitter)} onMouseLeave={() => setTwitter(!twitter)} className="m-2" bgColor="#ffc107" fgColor={ twitter ? "#1DA1F2" : "#000" } url="http://twitter.com/muhammedshah786" /></motion.div>
+     <motion.div initial={ {opacity: 0 , }} animate={{opacity: 1 , }} transition={ {delay: .2} } whileHover={{ scale: 1.4 }}> <SocialIcon  className="m-2" bgColor="#ffc107" fgColor="#000" url="https://github.com/Mshahas786"/></motion.div>
+     <motion.div initial={ {opacity: 0 , y: -90 }} animate={{opacity: 1 , y: 0 }} transition={ {delay: .5} } whileHover={{ scale: 1.4 }} onMouseEnter={() => setShow(!show)} onMouseLeave={() => setShow(!show)} ><SocialIcon className="m-2" bgColor="#ffc107" fgColor={ show ?  "#2164f4" : "#000" } url="https://www.linkedin.com/in/muhammed-shahas" /></motion.div>
+     <motion.div initial={ {opacity: 0 , y: -110 }} animate={{opacity: 1 , y: 0 }} transition={ {delay: 1} } whileHover={{ scale: 1.4 }} onMouseEnter={() => setTwitter(!twitter)} onMouseLeave={() => setTwitter(!twitter)} ><SocialIcon  className="m-2" bgColor="#ffc107" fgColor={ twitter ? "#1DA1F2" : "#000" } url="http://twitter.com/muhammedshah786" /></motion.div>
     </div>
   );
 };
