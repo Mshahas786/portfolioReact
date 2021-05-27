d7eb63c9acd177a71484b1273233ccb9068b7aa8
Mshahas786 <mshahas786@gmail.com>
2021-05-27 17:07:57 +1200

    hh

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
           initial={{ x:-100, scale:0, opacity: 0 }}
           animate={{ x:0, scale:1, opacity: 1 }}
           transition={{ delay: .1 }}
-          className="col-4 col-lg-3 p-0  bg-success ">
+          className="col-1 col-lg-3 p-0  bg-success ">
             <motion.img 
              initial={{ x:-100, opacity: 0 }}
              animate={{ x:0, opacity: 1 }}
@@ -30,13 +30,13 @@ const Home = () => {
           initial={{ x:-200, scale:0, opacity: 0 }}
           animate={{ x:0, scale:1, opacity: 1 }}
           transition={{ delay: .2 }}
-          className="col-8 col-lg-8 d-flex align-items-center bg-danger overflow-auto p-0 pl-2">
+          className="col-11 col-lg-8 d-flex align-items-center bg-danger overflow-auto p-0 pl-2">
             <motion.h1
             initial={{ x:-100, opacity: 0 }}
             animate={{ x:0, opacity: 1 }}
             transition={{ delay: .5 }}
-             className=" text-white  mt-5 text-uppercase ">
-              I'm a<br/> frontend <br/> developer
+             className=" text-white  mt-5  " style={{fontFamily:'Pattaya', fontSize:'4rem'}}>
+              Hello,<br/> I'm Muhammed<br/> The Front-end Developer.
             </motion.h1>
             <Link to="/Works">Work</Link>
           </motion.div>
